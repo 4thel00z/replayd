6ef054ccd841fefa627b2d1d8a481089ac82f461
4thel00z <4thel00z@gmail.com>
2020-08-22 13:27:22 +0200

    [master] Add models

---
+++ models.go
@@ -0,0 +1,19 @@
+package replayd
+
+import (
+	"net/http"
+	"net/url"
+)
+
+type Request struct {
+	HTTPVersion string      `json:"http_version"`
+	Host        string      `json:"host"`
+	Method      string      `json:"method"`
+	Headers     http.Header `json:"headers"`
+	Body        []byte      `json:"body"`
+	Form        url.Values  `json:"form"`
+}
+type StringWriterCloser interface {
+	WriteString(s string) (n int, err error)
+	Close() error
+}
