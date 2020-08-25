b7783fda672600db084fdd2c43455a0dd4639d27
4thel00z <4thel00z@gmail.com>
2020-08-25 19:39:49 +0200

    [master] Improve directory structure. Enforce pkg scheme

--- api.go
+++ api.go
@@ -4,12 +4,13 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/gob"
+	"github.com/4thel00z/replayd/pkg/libreplay"
 	"io"
 	"io/ioutil"
 	"net/http"
 )
 
-func Serialize(req Request) (string, error) {
+func Serialize(req libreplay.Request) (string, error) {
 	buffer := bytes.Buffer{}
 	encoder := gob.NewEncoder(&buffer)
 	err := encoder.Encode(req)
@@ -20,11 +21,11 @@ func Serialize(req Request) (string, error) {
 }
 
 // go binary decoder
-func Deserialize(str string) (Request, error) {
-	req := Request{}
+func Deserialize(str string) (libreplay.Request, error) {
+	req := libreplay.Request{}
 	rawBytes, err := base64.StdEncoding.DecodeString(str)
 	if err != nil {
-		return Request{}, err
+		return libreplay.Request{}, err
 	}
 	buffer := bytes.Buffer{}
 	buffer.Write(rawBytes)
@@ -32,12 +33,12 @@ func Deserialize(str string) (Request, error) {
 	err = decoder.Decode(&req)
 
 	if err != nil {
-		return Request{}, err
+		return libreplay.Request{}, err
 	}
 	return req, nil
 }
 
-func SaveRequest(req http.Request, stringWriterCloser StringWriterCloser) error {
+func SaveRequest(req http.Request, stringWriterCloser libreplay.StringWriterCloser) error {
 	defer func() {
 		_ = stringWriterCloser.Close()
 	}()
@@ -60,7 +61,7 @@ func SaveRequest(req http.Request, stringWriterCloser StringWriterCloser) error 
 		return err
 	}
 
-	payload, err := Serialize(Request{
+	payload, err := Serialize(libreplay.Request{
 		HTTPVersion: req.Proto,
 		Host:        req.Host,
 		Method:      req.Method,
@@ -84,15 +85,15 @@ func SaveRequest(req http.Request, stringWriterCloser StringWriterCloser) error 
 
 
 
-func RestoreRequest(reader io.Reader) (Request, error) {
+func RestoreRequest(reader io.Reader) (libreplay.Request, error) {
 	body, err := ioutil.ReadAll(reader)
 	if err != nil {
-		return Request{}, err
+		return libreplay.Request{}, err
 	}
 	// If this doesn't work, try
 	return Deserialize(string(body))
 }
 
 func Init() {
-	gob.Register(Request{})
+	gob.Register(libreplay.Request{})
 }
